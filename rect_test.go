352162963a19e3f793595544b09a1dd626841d77
Lee Witek <neomech@gmail.com>
2015-10-12 14:44:56 +0100

    use assert package for testing

--- rect_test.go
+++ rect_test.go
@@ -1,21 +1,21 @@
 package rect
 
 import (
-	"github.com/simulatedsimian/testbuddy"
+	"github.com/simulatedsimian/assert"
 	"testing"
 )
 
 func TestMinMax(t *testing.T) {
-	testbuddy.AssertEqual(t, min(1, 2), 1)
-	testbuddy.AssertEqual(t, min(2, 1), 1)
-	testbuddy.AssertEqual(t, min(1, 1), 1)
-	testbuddy.AssertEqual(t, max(1, 2), 2)
-	testbuddy.AssertEqual(t, max(2, 1), 2)
-	testbuddy.AssertEqual(t, max(2, 2), 2)
+	assert.Equal(t, min(1, 2), 1)
+	assert.Equal(t, min(2, 1), 1)
+	assert.Equal(t, min(1, 1), 1)
+	assert.Equal(t, max(1, 2), 2)
+	assert.Equal(t, max(2, 1), 2)
+	assert.Equal(t, max(2, 2), 2)
 }
 
 func TestCoord(t *testing.T) {
 	c := Vec{10, 20}
 	c.Add(Vec{1, 1})
-	testbuddy.AssertEqual(t, c, Vec{11, 21})
+	assert.Equal(t, c, Vec{11, 21})
 }
